e445fabe4aa4764911e2a8b18449dc2c881a890d
Usama503 <125597854+Usama503@users.noreply.github.com>
2025-04-27 02:35:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// This can be extended later for cart functionality
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+      alert("Item added to cart (dummy)");
+    });
+  });
